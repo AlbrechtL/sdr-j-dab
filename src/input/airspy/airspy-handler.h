9d685625d13d2ba0706218c9aad09c224e04cc24
janK <J.vanKatwijk@gmail.conf>
2015-11-20 16:12:21 +0100

    preparing for branching

--- src/input/airspy/airspy-handler.h
+++ src/input/airspy/airspy-handler.h
@@ -24,7 +24,7 @@
 #include	"ringbuffer.h"
 #include	"virtual-input.h"
 #include	"ui_airspy-widget.h"
-#include	"airspy_lib.h"
+#include	"airspy.h"
 
 extern "C"  {
 typedef	int (*pfn_airspy_init) (void);
